da6c8acd60c4850942a7f4111257d5c9ee3b9644
Russell Jarvis <rjjarvis@asu.edu>
2017-05-22 10:25:45 -0700

    dev changes to facilitate parallel ipython development of NEURON-7.4

---
+++ neuron-mpi-neuroml/test_fixed2.py
@@ -0,0 +1,22 @@
+#import mpi4py
+#from mpi4py import MPI
+
+import ipyparallel as ipp ;
+rc = ipp.Client(profile='chase');
+print('hello from before cpu ');
+print(rc.ids);
+
+from neuron import h
+pc = h.ParallelContext()
+#pc0 = MPI.COMM.Rank()
+id = int(pc.id())
+nhost = int(pc.nhost())
+
+print('I am a demonstration that NEURONs parallel context and ipyparallel are compossible, by showing the rank read outs are equal/same:')
+print('%d of %d %d' %(id, nhost))#, pc0))
+
+print((rc.ids))
+
+pc.runworker()
+pc.done()
+h.quit()
