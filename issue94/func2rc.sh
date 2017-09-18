1610426f9b8978feac7c27cb6e1f2adc5ed73219
Russell Jarvis <rjjarvis@asu.edu>
2017-09-18 08:10:44 -0700

    dev changes

--- issue94/func2rc.sh
+++ issue94/func2rc.sh
@@ -7,4 +7,13 @@ function sc(){
     ipcluster start -n 8 --profile=default --workdir=$1 &
     sleep 5
     python stdout_worker.py &
+    # explanation
+    # there is some ugly inconsistencies between the
+    # controller and the engines on ipyparallel
+    # basically the controllers fixed location of neuronunit is fixed to /home/jovyan/work/neuronunit/neuronunit
+    # this probably
+    # because PYTHONPATH was set to this early with ENV
+    # this is very static and it can't be updated
+    ln -s "`$1`../../neuronunit  /home/jovyan/work/neuronunit/neuronunit"
 }
+
